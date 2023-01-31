fe825d1d3ff11950079032021f023181a46c0fe9
StevanVS <stevanvs@outlook.com>
2023-01-30 21:35:33 -0500

    Agregar archivos de proyecto.

---
+++ Models/ApiRestContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace prueba_API.Models;
+
+public partial class ApiRestContext : DbContext
+{
+    public ApiRestContext()
+    {
+    }
+
+    public ApiRestContext(DbContextOptions<ApiRestContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Persona> Personas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=STEVAN-ACER;Database=ApiRest;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Persona>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("pk_personas");
+
+            entity.ToTable("personas");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(25)
+                .IsUnicode(false)
+                .HasColumnName("apellido");
+            entity.Property(e => e.Estatura).HasColumnName("estatura");
+            entity.Property(e => e.FechaNacimiento)
+                .HasColumnType("date")
+                .HasColumnName("fechaNacimiento");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(25)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+            entity.Property(e => e.Ocupacion)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("ocupacion");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace prueba_API.Models;
+
+public partial class Persona
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Apellido { get; set; } = null!;
+
+    public DateTime FechaNacimiento { get; set; }
+
+    public double Estatura { get; set; }
+
+    public string? Ocupacion { get; set; }
+}
